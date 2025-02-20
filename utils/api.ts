62002b9ce4bd54cb115f7fe8cb933e76c6c3ee95
minhvu278 <dominhvu278@gmail.com>
2025-02-20 23:26:28 +0700

    fix port 4000

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'http://103.221.221.110:3000';
+const API_BASE_URL = 'http://103.221.221.110:4000';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
