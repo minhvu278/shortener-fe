0cb91834d456f6d12822f41a4f14e31830c1f8ce
minhvu278 <dominhvu278@gmail.com>
2025-03-01 23:34:53 +0700

    fix run build

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/lib/theme";
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/header";
 import Footer from "@/components/layout/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
